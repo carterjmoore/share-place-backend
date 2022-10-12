02ab89bb0036de6e9467d1cf2dab52c10f188517
Carter Moore <carterjmoore02@gmail.com>
2022-10-12 16:39:56 -0300

    add the rest of the places routes

--- app.js
+++ app.js
@@ -2,10 +2,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const placesRoutes = require('./routes/places-routes');
+const HttpError = require('./models/http-error');
 
 const app = express();
 
+app.use(bodyParser.json());
+
 app.use('/api/places', placesRoutes);
+
+// Handle routes that don't exist
+app.use((req, res, next) => {
+  next(new HttpError('Could not find this route', 404));
+});
 
 app.use((error, req, res, next) => {
   if (res.headerSent) {
--- routes/places-routes.js
+++ routes/places-routes.js
@@ -1,47 +1,17 @@
 const express = require('express');
 
-const HttpError = require('../models/http-error');
+const placesControllers = require('../controllers/places-controllers');
 
 const router = express.Router();
 
-const DUMMY_PLACES = [
-  {
-    id: 'p1',
-    title: 'Empire State Building',
-    description: 'One of the most famous sky scrapers in the world!',
-    location: {
-      lat: 40.7484474,
-      lng: -73.9871516,
-    },
-    address: '20 W 34th St, New York, NY 10001',
-    creator: 'u1',
-  },
-];
+router.get('/:pid', placesControllers.getPlaceById);
 
-router.get('/:pid', (req, res, next) => {
-  const placeId = req.params.pid;
-  const place = DUMMY_PLACES.find((p) => p.id === placeId);
+router.get('/user/:uid', placesControllers.getPlacesByUserId);
 
-  if (!place) {
-    return next(
-      new HttpError('Could not find a place for the provided place id.', 404)
-    );
-  }
+router.patch('/:pid', placesControllers.updatePlace);
 
-  res.json({ place });
-});
+router.delete('/:pid', placesControllers.deletePlace);
 
-router.get('/user/:uid', (req, res, next) => {
-  const userId = req.params.uid;
-  const place = DUMMY_PLACES.find((p) => p.creator === userId);
-
-  if (!place) {
-    return next(
-      new HttpError('Could not find a place for the provided user id.', 404)
-    );
-  }
-
-  res.json({ place });
-});
+router.post('/', placesControllers.createPlace);
 
 module.exports = router;
