9ac300c6d3a8f8910ef56e912670f3b6d089e2bc
Carter Moore <carterjmoore02@gmail.com>
2022-10-12 18:45:53 -0300

    add mongoose connection

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const placesRoutes = require('./routes/places-routes');
 const usersRoutes = require('./routes/users-routes');
@@ -27,4 +28,13 @@ app.use((error, req, res, next) => {
   res.json({ message: error.message || 'An unknown error occured!' });
 });
 
-app.listen(5000);
+mongoose
+  .connect(
+    'mongodb+srv://carterjmoore:ECY7lJbPzwk8yDi1@shareplace.w9aakxl.mongodb.net/places?retryWrites=true&w=majority'
+  )
+  .then(() => {
+    app.listen(5000);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
