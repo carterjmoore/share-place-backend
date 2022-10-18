8c84241c589f2a9e92200fc750dd263747de9e63
Carter Moore <carterjmoore02@gmail.com>
2022-10-18 19:41:21 -0400

    setup for deployment to fly

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ mongoose
     `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@shareplace.w9aakxl.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
   )
   .then(() => {
-    app.listen(process.env.PORT || 5000);
+    app.listen(process.env.PORT || 8080);
   })
   .catch((error) => {
     console.log(error);
